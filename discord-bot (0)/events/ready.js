a1cabdb8ef4d1663b2274726f1cdf39746769754
hghgh57 <rizzbut3@gmail.com>
2026-08-11 17:58:44 +1000

    Update ready.js

--- discord-bot (0)/events/ready.js
+++ discord-bot (0)/events/ready.js
@@ -1,7 +1,18 @@
+const { ActivityType } = require('discord.js');
+const config = require('../config.json');
+
 module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
     console.log(`✅ Logged in as ${client.user.tag}`);
+
+    const statusText = config.botStatus;
+    if (statusText) {
+      client.user.setPresence({
+        activities: [{ name: statusText, type: ActivityType.Custom }],
+        status: 'online',
+      });
+    }
   },
 };
